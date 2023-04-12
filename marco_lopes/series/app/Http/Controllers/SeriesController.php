e5a698081dfd32b82b0a5a5990b4e89de7a9955e
marcoAL2000 <marcoantoniolopes2016@gmail.com>
2023-04-11 22:18:26 -0300

    Crud Series

---
+++ marco_lopes/series/app/Http/Controllers/SeriesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Serie;
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function index()
+    {
+        $series = Serie::get();
+        return view('series.index', compact('series'));
+    }
+
+    public function create()
+    {
+        return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        $serie = Serie::create($request->all());
+        return redirect()->route('list_series');
+    }
+}
